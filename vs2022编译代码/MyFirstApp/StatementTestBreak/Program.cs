62f3287c114bfae78747d785babbe346d74be231
HY10 <1670748498@QQ.COM>
2023-10-19 17:35:54 +0800

    testNEWPC

---
+++ vs2022编译代码/MyFirstApp/StatementTestBreak/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace StatementTestBreak
+{
+    //testgitnew
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int sum = 0;
+            int x = 0;
+            int error = 0;
+            do
+            {
+                error = 0;//初始化error标识，避免先前赋值1造成死循环
+                Console.WriteLine("input  first number");
+                string str1 = Console.ReadLine();
+                try
+                {
+
+                    x = int.Parse(str1);
+
+                }
+                catch
+                {
+                    if (str1.ToLower() == "end")
+                    {
+                        break;
+                    }
+                    error = 1;
+                    Console.WriteLine("restart input");
+                    continue;
+
+                    //继续循环语句，跳转至do的}最后一个大括号，因此还会执行while的判断，因此需要加入error
+                }
+
+                //终止break
+               
+                Console.WriteLine("input second number");
+                string str2 = Console.ReadLine();
+                int y = 0;
+                try
+                {
+                    y = int.Parse(str2);
+                }
+                catch
+                {
+                    error = 1;
+                    Console.WriteLine("restart second input");
+                    continue;
+
+                }
+                sum = x + y;
+                if (sum == 100)
+                {
+                    score++;
+                }
+
+            }
+            while (sum == 100 || error == 1);
+            Console.WriteLine("your score is {0}", score);
+
+
+            int counter=0;
+            while (counter<10)
+            {
+                Console.WriteLine("hello");
+                counter++;  
+            }
+            for (int counter2 = 0; counter2 < 10; counter2++)
+            {
+                Console.WriteLine("helloworld");
+            }
+        }
+    }
+}
